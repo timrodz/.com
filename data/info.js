6d4521a4cf645ebaeb4942bcdd8998148f7f1f14
Juan Alejandro Morais <timrodz@icloud.com>
2019-10-15 21:30:08 +1300

    Update data/social, data/info

--- data/info.js
+++ data/info.js
@@ -1,5 +1,7 @@
 module.exports = {
   name: 'Juan Alejandro Morais',
   title: 'Full Stack Developer',
+  portfolio: 'My background as a game developer has empowered my programming career — I am always up for a challenge, no matter the stack.',
+  projects: 'I am always looking for new ways to learn & improve my skills — Contributing to Open Source is a great way of achieving this.',
   about: 'I have a passion for game & data development. My mission is to bring agile processes to Data Engineering and Game Development pipelines.'
 };
