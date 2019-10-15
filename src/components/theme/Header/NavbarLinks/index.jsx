f495558cb8ab6316a591150c8c7c88619cc54595
Juan Alejandro Morais <timrodz@icloud.com>
2019-10-15 21:30:37 +1300

    Format components/theme/Header/NavbarLinks

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import { Wrapper } from './styles';
+import {Wrapper} from './styles';
 
-const NavbarLinks = ({ desktop }) => (
+const NavbarLinks = ({desktop}) => (
   <Wrapper desktop={desktop}>
     <AnchorLink href="#portfolio">Portfolio</AnchorLink>
     <AnchorLink href="#projects">Open Source</AnchorLink>
     <AnchorLink href="#about">About</AnchorLink>
-    <AnchorLink href="#contact">Contact</AnchorLink>
   </Wrapper>
 );
 
