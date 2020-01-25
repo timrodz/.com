417008f25fb537065c34fae1db1f6a7ea0f1151b
Juan Alejandro Morais <timrodz@icloud.com>
2020-01-25 20:43:59 +1300

    Add 'Blog Posts' to NavbarLinks

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -6,6 +6,7 @@ const NavbarLinks = ({desktop}) => (
   <Wrapper desktop={desktop}>
     <AnchorLink href="#portfolio">Portfolio</AnchorLink>
     <AnchorLink href="#projects">Open Source</AnchorLink>
+    <AnchorLink href="#posts">Blog Posts</AnchorLink>
     <AnchorLink href="#about">About</AnchorLink>
   </Wrapper>
 );
