91f7a3a6348a364b15929439a46bd65e1acbab7e
Juan Alejandro Morais <timrodz@users.noreply.github.com>
2020-02-07 10:00:00 +1300

    Update info.js
    Fix typo: `fullfil` -> `fulfill`

--- data/info.js
+++ data/info.js
@@ -8,6 +8,6 @@ module.exports = {
   posts:
     'I love sharing my knowledge with others. My content is centered around an audience starting with Web Technologies and Game Development.',
   about:
-    'I am a Software Engineer and a strong advocate for success as the product of great leadership, mentorship, team work, and active collaboration — This is what you can expect from me. I specialize in building Full Stack Applications that help businesses and individuals fullfil their dreams and reach their goals. My front-end (JS) stack encompasses Gatsby, React, and GraphQL. For back-end (Python) I work with Flask, Pandas, Celery, AWS, and SQL.',
+    'I am a Software Engineer and a strong advocate for success as the product of great leadership, mentorship, team work, and active collaboration — This is what you can expect from me. I specialize in building Full Stack Applications that help businesses and individuals fulfill their dreams and reach their goals. My front-end (JS) stack encompasses Gatsby, React, and GraphQL. For back-end (Python) I work with Flask, Pandas, Celery, AWS, and SQL.',
   maxPostsToShow: 3,
 };
